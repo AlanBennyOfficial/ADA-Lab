8b0d9bb1600a5a8780c08eb95993874f0460b006
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2026-02-13 10:20:52 +0530

    Add files via upload

---
+++ BinaryTreeTraversal.py
@@ -0,0 +1,74 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.data, end=" ")
+        inorder(root.right)
+        
+def preorder(root):
+    if root:
+        print(root.data , end=" ")
+        preorder(root.left)
+        preorder(root.right)
+
+def postorder(root):
+    if root:
+        print(root.data, end=" ")
+        postorder(root.left)
+        postorder(root.right)
+
+# Level order traversal (BFS)
+
+def level_order(root):
+    if root is None:
+        return
+    
+    queue = []
+    queue.append(root)
+
+    while queue:
+        current = queue.pop(0)
+        print(current.data, end=" ")
+
+        if current.left:
+            queue.append(current.left)
+
+        if current.right:
+            queue.append(current.right)
+
+# -----------------------------
+# Creating a Sample Binary Tree
+# -----------------------------
+#
+#          1
+#         / \
+#        2   3
+#       / \
+#      4   5
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+# ---------------------------
+# Calling Traversal Functions
+# ---------------------------
+
+print("Inorder Traversal: ")
+inorder(root)
+
+print("\nPreorder Traveral: ")
+preorder(root)
+
+print("\nPostorder Traversal: ")
+postorder(root)
+
+print("\nLevel order Traversal: ")
+level_order(root)
